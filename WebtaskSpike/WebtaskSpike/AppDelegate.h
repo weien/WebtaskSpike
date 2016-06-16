f6ba1bea5a6d110fc8936e02a8cba511eef6e51c
Weien Wang <weienw@gmail.com>
2016-06-16 18:42:48 -0500

    init

---
+++ WebtaskSpike/WebtaskSpike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebtaskSpike
+//
+//  Created by Weien Wang on 6/16/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebtaskSpike/WebtaskSpike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebtaskSpike
+//
+//  Created by Weien Wang on 6/16/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
