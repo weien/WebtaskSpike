4fddc179f03745409dec2db6221da12cb62688e2
Weien Wang <weienw@gmail.com>
2016-06-16 19:30:37 -0500

    [ADD] basic functionality for displaying text in transcriptView when user enters something

--- WebtaskSpike/WebtaskSpike/ViewController.m
+++ WebtaskSpike/WebtaskSpike/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "JRTranscriptView.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet JRTranscriptView *transcriptView;
 @property (strong, nonatomic) IBOutlet UITextField *mainTextField;
 
@@ -21,5 +21,34 @@
     [super viewDidLoad];
     
 }
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.mainTextField becomeFirstResponder];
+    self.mainTextField.delegate = self;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSAttributedString* styledString = [self userAttributedStringWithText:textField.text];
+    [self.transcriptView appendAttributedString:styledString];
+    textField.text = @"";
+    return YES;
+}
+
+- (NSAttributedString*) userAttributedStringWithText:(NSString*)text {
+    NSString* formattedString = [NSString stringWithFormat:@"\n%@",text];
+    NSMutableAttributedString* styledString = [[NSMutableAttributedString alloc] initWithString:formattedString];
+    return styledString;
+}
+
+- (NSAttributedString*) botAttributedStringWithText:(NSString*)text {
+    NSMutableAttributedString* styledString = [[NSMutableAttributedString alloc] initWithString:text];
+    NSDictionary* attributes = @{NSForegroundColorAttributeName : [UIColor redColor]};
+    [styledString addAttributes:attributes range:NSMakeRange(0, text.length)];
+    return styledString;
+}
+
+
 
 @end
