a07f47caf737cac7567cb07aac28e2d288442fd6
Weien Wang <weienw@gmail.com>
2016-06-16 21:14:54 -0500

    [ADD] basic round trip to simple Webtask and back

--- WebtaskSpike/WebtaskSpike/ViewController.m
+++ WebtaskSpike/WebtaskSpike/ViewController.m
@@ -30,10 +30,22 @@
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    NSAttributedString* styledString = [self userAttributedStringWithText:textField.text];
-    [self.transcriptView appendAttributedString:styledString];
-    textField.text = @"";
-    return YES;
+    NSString* cleanString = [textField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    if (cleanString.length > 0) {
+        NSAttributedString* styledString = [self userAttributedStringWithText:textField.text];
+        [self.transcriptView appendAttributedString:styledString];
+        [self tellWebtaskBot:textField.text callback:^(NSString *reply) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSAttributedString* styledReply = [self botAttributedStringWithText:reply];
+                [self.transcriptView appendAttributedString:styledReply];
+            });
+        }];
+        textField.text = @"";
+        return YES;
+    }
+    else {
+        return NO;
+    }
 }
 
 - (NSAttributedString*) userAttributedStringWithText:(NSString*)text {
@@ -43,12 +55,32 @@
 }
 
 - (NSAttributedString*) botAttributedStringWithText:(NSString*)text {
-    NSMutableAttributedString* styledString = [[NSMutableAttributedString alloc] initWithString:text];
+    NSString* formattedString = [NSString stringWithFormat:@"\n%@",text];
+    NSMutableAttributedString* styledString = [[NSMutableAttributedString alloc] initWithString:formattedString];
     NSDictionary* attributes = @{NSForegroundColorAttributeName : [UIColor redColor]};
     [styledString addAttributes:attributes range:NSMakeRange(0, text.length)];
     return styledString;
 }
 
-
+- (void) tellWebtaskBot:(NSString*)text callback:(void (^)(NSString* reply))callback {
+    NSString* fullURLString = [@"https://webtask.it.auth0.com/api/run/wt-weienw-gmail_com-0/hello?text=" stringByAppendingString:text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:fullURLString]];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask* task = [session dataTaskWithRequest:request
+                                        completionHandler:^(NSData * _Nullable data,
+                                                            NSURLResponse * _Nullable response,
+                                                            NSError * _Nullable error) {
+                                            if (error) {
+                                                NSLog(@"Webtask error is %@", error);
+                                                callback(nil);
+                                            }
+                                            else {
+                                                NSString* replyString = [[NSString alloc] initWithData:data
+                                                                                               encoding:NSUTF8StringEncoding];
+                                                callback(replyString);
+                                            }
+                                        }];
+    [task resume];
+}
 
 @end
